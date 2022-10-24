b3297f5b9248024a1bbf9fccc2e9f6c1620bb57a
PORTALSURFER <portalsurfer@gmail.com>
2022-10-24 11:52:58 +0200

    all objects receive all messages

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,108 @@
 use std::time::Duration;
+type Message = Box<dyn Command>;
 
-struct SomeObject {}
+// type Receiver = tokio::sync::mpsc::UnboundedReceiver<Message>;
+// type Sender = tokio::sync::mpsc::UnboundedSender<Message>;
 
-impl SomeObject {
-    fn new() -> Self {
-        Self {}
+type ReceiverObject = Box<dyn Subscriber>;
+
+trait Command {
+    fn get_name(&self) -> String;
+}
+
+#[derive(Debug)]
+struct SomeObject;
+
+impl Subscriber for SomeObject {
+    fn process(&self, message: Message) {
+        println!("Processing some_object, ({})", message.get_name());
+    }
+
+    fn get_name(&self) -> String {
+        String::from("SomeObject")
+    }
+}
+pub enum SomeObjectCommand {
+    One,
+    Two,
+}
+impl Command for SomeObjectCommand {
+    fn get_name(&self) -> String {
+        String::from("SomeObjectCommand")
     }
 }
 
-impl Subscriber for SomeObject {
-    fn process(&self) {
-        print!("Processing someobject")
+#[derive(Debug)]
+struct SomeOtherObject;
+
+impl Subscriber for SomeOtherObject {
+    fn process(&self, message: Message) {
+        println!("Processing some_other_object, ({})", message.get_name());
+    }
+
+    fn get_name(&self) -> String {
+        String::from("SomeOtherObject")
+    }
+}
+pub enum SomeOtherObjectCommand {
+    OneOther,
+    TwoOther,
+}
+impl Command for SomeOtherObjectCommand {
+    fn get_name(&self) -> String {
+        String::from("SomeOtherObjectCommand")
     }
 }
 
 trait Subscriber {
-    fn process(&self);
+    fn process(&self, message: Message);
+    fn get_name(&self) -> String;
 }
 
-struct MessageProcessor<I>
-where
-    I: Subscriber,
-{
-    subscribers: Vec<I>,
+struct MessageProcessor {
+    subscribers: Vec<ReceiverObject>,
 }
 
 fn main() {
-    print!("Running");
+    println!("Running");
 
     let runtime = tokio::runtime::Runtime::new().unwrap();
 
     runtime.block_on(async move {
-        let some_obj = SomeObject::new();
+        let some_obj = SomeObject;
+        let some_other_object = SomeOtherObject;
         let mut msg_proc = MessageProcessor::new();
 
-        msg_proc.register(some_obj);
-        print!("Entering processors..");
+        msg_proc.register(Box::new(some_obj));
+        msg_proc.register(Box::new(some_other_object));
+
+        println!("Entering processors..");
         loop {
-            msg_proc.run().await
+            msg_proc.run().await;
         }
     });
 }
 
-impl<I> MessageProcessor<I>
-where
-    I: Subscriber,
-{
+// concrete types
+impl MessageProcessor {
     pub fn new() -> Self {
         let subscribers = vec![];
 
         Self { subscribers }
     }
 
-    pub fn register(&mut self, item: I)
-    where
-        I: Subscriber,
-    {
+    pub fn register(&mut self, item: ReceiverObject) {
         self.subscribers.push(item);
     }
 
     pub async fn run(&self) {
-        tokio::spawn(async move {
-            loop {
-                tokio::time::sleep(Duration::from_millis(500)).await;
-                print!("World");
+        loop {
+            tokio::time::sleep(Duration::from_secs(1)).await;
+            for subscriber in &self.subscribers {
+                println!("send msg to ({})", subscriber.get_name());
+                let message = Box::new(self::SomeObjectCommand::One);
+                subscriber.process(message);
             }
-        });
+        }
     }
 }
