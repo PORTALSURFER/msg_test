6e61c1ba55dc8b8584a72d492d3e2f807f121bbe
PORTALSURFER <contact@portalsurfer.com>
2022-10-25 17:24:28 +0200

    Update static_pass.rs

--- src/static_pass.rs
+++ src/static_pass.rs
@@ -1,119 +1,111 @@
-#[derive(Debug)]
-enum SomeCommand {
-    One,
-    Two,
-}
+use std::time::Duration;
 
-#[derive(Debug)]
-enum SomeOtherCommand {
-    OneOther,
-    TwoOther,
-}
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum CommandTypes {
-    SomeCommand(SomeCommand),
-    SomeOtherCommand(SomeOtherCommand),
+    SomeCommand,
+    SomeOtherCommand,
 }
 
 impl CommandTypeCollection for CommandTypes {}
 trait CommandTypeCollection {}
 
 #[derive(Debug)]
-struct SomeObject;
+struct SomeObject<C> {
+    receiver: Option<tokio::sync::broadcast::Receiver<C>>,
+}
 
-impl Subscriber for SomeObject {
-    fn process(&self, command: &CommandTypes) {
-        match command {
-            CommandTypes::SomeCommand(command) => {
-                println!("{:?} is processing: {:?}", self, command);
-            }
-            _ => {}
-        }
+impl<C> SomeObject<C> {
+    fn new() -> Self {
+        Self { receiver: None }
     }
+    async fn run() {}
+}
+
+impl<C> Subscriber for SomeObject<C> {
+    type CommandType = CommandTypes;
 }
 
 #[derive(Debug)]
-struct SomeOtherObject;
+struct SomeOtherObject<C> {
+    receiver: Option<tokio::sync::broadcast::Receiver<C>>,
+}
 
-impl Subscriber for SomeOtherObject {
-    fn process(&self, command: &CommandTypes) {
-        match command {
-            CommandTypes::SomeOtherCommand(command) => {
-                println!("{:?} is processing: {:?}", self, command);
-            }
-            _ => {}
-        }
+impl<C> SomeOtherObject<C> {
+    fn new() -> Self {
+        Self { receiver: None }
     }
 }
 
-trait Message {}
+impl<C> Subscriber for SomeOtherObject<C> {
+    type CommandType = CommandTypes;
+}
 
 trait Subscriber {
-    fn process(&self, command: &CommandTypes);
+    type CommandType;
 }
 
-struct MessageProcessor {
-    subscribers: Vec<Box<dyn Subscriber>>,
+struct MessageProcessor<C> {
+    subscribers: Vec<Box<dyn Subscriber<CommandType = C>>>,
+    sender: tokio::sync::broadcast::Sender<C>,
+    receiver: tokio::sync::mpsc::UnboundedReceiver<C>,
 }
 
-impl MessageProcessor {
-    fn new() -> Self {
+impl<C: Clone> MessageProcessor<C> {
+    fn new(receiver: tokio::sync::mpsc::UnboundedReceiver<C>) -> Self {
+        let (sender, _) = tokio::sync::broadcast::channel(8);
+
         Self {
             subscribers: Vec::new(),
+            sender,
+            receiver,
         }
     }
 
-    fn register(&mut self, item: Box<dyn Subscriber>) {
+    fn register(&mut self, item: Box<dyn Subscriber<CommandType = C>>) {
         self.subscribers.push(item)
     }
 
     async fn run(self) {
-        loop {
-            println!("STATIC: processing commands..");
+        loop {}
+    }
+}
 
-            // cmd
-            tokio::time::sleep(std::time::Duration::from_millis(500)).await;
-            let command = CommandTypes::SomeCommand(SomeCommand::One);
-            for subscriber in &self.subscribers {
-                subscriber.process(&command);
-            }
+struct UI<C> {
+    sender: tokio::sync::mpsc::UnboundedSender<C>,
+}
 
-            // cmd
-            tokio::time::sleep(std::time::Duration::from_millis(750)).await;
-            let command = CommandTypes::SomeCommand(SomeCommand::Two);
-            for subscriber in &self.subscribers {
-                subscriber.process(&command);
-            }
+impl<C> UI<C> {
+    fn new(sender: tokio::sync::mpsc::UnboundedSender<C>) -> Self {
+        Self { sender }
+    }
 
-            // cmd
-            tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
-            let command = CommandTypes::SomeOtherCommand(SomeOtherCommand::OneOther);
-            for subscriber in &self.subscribers {
-                subscriber.process(&command);
-            }
-            // cmd
-            tokio::time::sleep(std::time::Duration::from_millis(1250)).await;
-            let command = CommandTypes::SomeOtherCommand(SomeOtherCommand::TwoOther);
-            for subscriber in &self.subscribers {
-                subscriber.process(&command);
-            }
-        }
+    async fn run(self) {
+        println!("start ui");
+        tokio::time::sleep(Duration::from_millis(500)).await;
+        // self.sender.send(CommandTypes::SomeCommand);
     }
 }
 
 pub fn static_pass() {
     let rt = tokio::runtime::Runtime::new().unwrap();
 
-    let some_object = SomeObject;
-    let some_other_object = SomeOtherObject;
+    let (sender, receiver) = tokio::sync::mpsc::unbounded_channel();
 
-    let mut message_processor = MessageProcessor::new();
+    let ui = UI::new(sender);
+
+    let some_object: SomeObject<CommandTypes> = SomeObject::new();
+    let some_other_object: SomeOtherObject<CommandTypes> = SomeOtherObject::new();
+
+    let mut message_processor = MessageProcessor::new(receiver);
 
     message_processor.register(Box::new(some_object));
     message_processor.register(Box::new(some_other_object));
 
     rt.block_on(async {
+        tokio::spawn(async {
+            ui.run().await;
+        });
+
         message_processor.run().await;
     })
 }
